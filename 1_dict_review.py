f0b0f80adc40dddb4eb258819ec6ae08bc22471b
maria-manuel <mariamanuel.tech@gmail.com>
2023-11-20 16:42:26 -0800

    print dictionary item w/i list

--- 1_dict_review.py
+++ 1_dict_review.py
@@ -99,10 +99,10 @@ results = {
 print(results['users'][0]['name'])
 
 # 2. Print the name of 'Mary Bucket'
-# print(results['users'])
+print(results['users'][3]['name'])
 
 # 3. Print the phone number of 'Father Kimmy Santiago'
-# print(results)
+print(results['users'][1]['contact']['phone'])
 
 # 4. Print the bio of Dame Daniel Eathorse
 # print(results)
